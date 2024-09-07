a9665724e3cbaf5b0a1a47b6833c99295c8f5dd4
FamousLuisin <lufimelo@outlook.com>
2024-09-07 20:25:30 -0300

    Criando user_request / user_response e aplicando validações

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,7 +1,24 @@
 package controller
 
 import (
+	"fmt"
+	"net/http"
+
+	"github.com/FamousLuisin/api-go/src/config/rest_err"
+	"github.com/FamousLuisin/api-go/src/controller/model/request"
 	"github.com/gin-gonic/gin"
 )
 
-func CreateUser(c *gin.Context) {}
+func CreateUser(c *gin.Context) {
+
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(fmt.Sprintf("There are some incorrect filds, error=%s", err.Error()))
+
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	c.JSON(http.StatusOK, userRequest)
+}
